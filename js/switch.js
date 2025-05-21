27869f720155fe5907738788ebf8cce0b4f8e488
fatima arifeen <fatima.arifeen24@gmail.com>
2025-05-21 15:19:24 +0600

    attributes starting with a

--- js/switch.js
+++ js/switch.js
@@ -57,3 +57,92 @@ switch (calculatingOperator) {
         console.log("Invalid operator")
         break;
 }
+
+// month days:
+let month = "march";
+switch (month) {
+    case "march":
+    case "may":
+    case "june":
+    case "september":
+    case "november":
+        console.log("this month has 30 days.")
+        break;
+    case "february":
+        console.log("this month has 28 or 29 days.")
+        break;
+
+    default:
+        console.log("this month has 31 days.")
+        break;
+}
+// Week Type (Weekend or Weekday):
+
+let weekday = "saturday";
+
+switch (weekday) {
+    case "friday":
+    case "saturday":
+        console.log("hurray it's weekend")
+        break;
+
+    default:
+        console.log("oh it's work day")
+        break;
+}
+
+// Number to Word:
+
+let number = "4";
+switch (number) {
+    case 1:
+        console.log("one")
+        break;
+    case 2:
+        console.log("two")
+        break;
+    case 3:
+        console.log("three")
+        break;
+    case 4:
+        console.log("four")
+        break;
+    case 5:
+        console.log("five")
+        break;
+
+    default:
+        console.log("out of range")
+        break;
+}
+
+//  Character Type (Vowel or Consonant):
+let character = "a";
+
+switch (character) {
+    case "a":
+    case "e":
+    case "o":
+    case "i":
+    case "u":
+        console.log("it's a vowel.")
+        break;
+
+    default:
+        console.log("it's a consonant")
+        break;
+}
+
+// Currency Converter:
+
+let currency = "usd";
+let amount = "1";
+
+switch (currency) {
+    case "usd":
+        
+        break;
+
+    default:
+        break;
+}
